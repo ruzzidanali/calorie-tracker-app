fbfa470d5c9ca6e549b8640a7a8b00e283e9450f
ruzzidanali <ruzzidan25@gmail.com>
2026-02-26 12:54:11 +0800

    Hidden

--- src/services/supabaseConfig.js
+++ src/services/supabaseConfig.js
@@ -1,12 +1,3 @@
-// export const SUPABASE_URL = process.env.EXPO_PUBLIC_SUPABASE_URL;
-// export const SUPABASE_ANON_KEY = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
-
-// export const SUPABASE_URL = "https://gzsblpavwzewhvihujct.supabase.co";
-// export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6c2JscGF2d3pld2h2aWh1amN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3OTE3ODcsImV4cCI6MjA4NjM2Nzc4N30.Uf6yxQndVsp1zzud2xA7g2dDDr6IjeXVy4f2jFCSszk";
-
-// https://gzsblpavwzewhvihujct.supabase.co
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6c2JscGF2d3pld2h2aWh1amN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3OTE3ODcsImV4cCI6MjA4NjM2Nzc4N30.Uf6yxQndVsp1zzud2xA7g2dDDr6IjeXVy4f2jFCSszk
-
 import { ENV } from "../config/env";
 
 export const SUPABASE_URL = ENV.SUPABASE_URL;
